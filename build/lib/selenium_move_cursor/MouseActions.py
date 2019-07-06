d65b4863d5d0d994c91364d45f16cbdcef002733
smirad91 <smirad91@gmail.com>
2019-07-06 20:41:35 +0200

    Built package

--- build/lib/selenium_move_cursor/MouseActions.py
+++ build/lib/selenium_move_cursor/MouseActions.py
@@ -3,23 +3,22 @@ import pyautogui
 from win32api import GetSystemMetrics
 
 
-
-def move_to_element_chrome(driver, element, displayScaling=125):
+def move_to_element_chrome(driver, element, display_scaling=125):
     """
     Move cursor to top right corner of element
 
     :param driver: WebDriver
     :param element: WebElement
-    :param displayScaling: int
+    :param display_scaling: int
     :return:
     """
-    tab_bar = int(72 * displayScaling / 100) #71 is chrome tab and address bar height when scaling is 100%
-    x = _get_location_in_pixel(driver, element, tab_bar)["x"]
-    y = _get_location_in_pixel(driver, element, tab_bar)["y"] + tab_bar
+    tab_bar = int(72 * display_scaling / 100) #71 is chrome tab and address bar height when scaling is 100%
+    x = _get_location_in_pixel(driver, element, tab_bar, display_scaling)["x"]
+    y = _get_location_in_pixel(driver, element, tab_bar, display_scaling)["y"] + tab_bar
     pyautogui.moveTo(x, y, duration=1)
 
 
-def _get_location_in_pixel(driver, element, tab_bar_height):
+def _get_location_in_pixel(driver, element, tab_bar_height, scaling):
     """
     Get location of element that is shown in browser
 
@@ -29,16 +28,20 @@ def _get_location_in_pixel(driver, element, tab_bar_height):
     """
     x_location_inside_of_browser = element.location["x"]
     y_location_inside_of_browser = element.location["y"]
-    scroll_size = 0
+    scroll_size = 16 * scaling / 100
+    vertical_scroll_size = 0
+    horizontal_scroll_size = 0
     if _is_vertical_scroll_on_screen(driver):
-        pass
-    x_location_in_pixels = x_location_inside_of_browser*(GetSystemMetrics(0)-scroll_size)/(int(driver.find_element_by_tag_name("html").get_attribute("clientWidth")))
+        vertical_scroll_size = scroll_size
+    # if _is_horizontal_scroll_on_screen(driver):
+    #     horizontal_scroll_size = scroll_size
+    x_location_in_pixels = (x_location_inside_of_browser + element.size["width"]/2)*(GetSystemMetrics(0)-vertical_scroll_size)/(int(driver.find_element_by_tag_name("html").get_attribute("clientWidth")))
     monitor_info = GetMonitorInfo(MonitorFromPoint((0, 0)))
     monitor_area = monitor_info.get("Monitor")
     work_area = monitor_info.get("Work")
     task_bar = monitor_area[3] - work_area[3]
     hidden_pixels = driver.execute_script("return window.pageYOffset")
-    y_location_in_pixels = (y_location_inside_of_browser-hidden_pixels)*(GetSystemMetrics(1)-tab_bar_height-task_bar)/int(driver.find_element_by_tag_name("html").get_attribute("clientHeight"))
+    y_location_in_pixels = (y_location_inside_of_browser+element.size["height"]/2-hidden_pixels)*(GetSystemMetrics(1)-tab_bar_height-task_bar - horizontal_scroll_size)/int(driver.find_element_by_tag_name("html").get_attribute("clientHeight"))
 
     return {'x': x_location_in_pixels, 'y': y_location_in_pixels}
 
@@ -56,3 +59,19 @@ def _is_vertical_scroll_on_screen(driver):
     if body_height > inner_height:
         vertical_scroll = True
     return vertical_scroll
+
+
+def _is_horizontal_scroll_on_screen(driver):
+    """
+    Checks if scroll is shown
+
+    :param driver: WebDriver
+    :return: bool
+    """
+    horizontal_scroll = False
+    body_width = driver.execute_script("return document.getElementsByTagName('body')[0].clientWidth")
+    inner_width = driver.execute_script("return window.innerWidth")
+    if body_width > inner_width:
+        horizontal_scroll = True
+    return horizontal_scroll
+
