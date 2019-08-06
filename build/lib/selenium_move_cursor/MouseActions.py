ae7a2ad1fbb40e31476619135b910f1c7cbefe23
smirad91 <smirad91@gmail.com>
2019-08-06 13:14:28 +0200

    update

--- build/lib/selenium_move_cursor/MouseActions.py
+++ build/lib/selenium_move_cursor/MouseActions.py
@@ -3,7 +3,7 @@ import pyautogui
 from win32api import GetSystemMetrics
 
 
-def move_to_element_chrome(driver, element, display_scaling):
+def move_to_element_chrome(driver, element, info_bar_shown=True, display_scaling=100):
     """
     Move cursor to middle of element
 
@@ -16,7 +16,10 @@ def move_to_element_chrome(driver, element, display_scaling):
     :type display_scaling: int
     :return:
     """
-    tab_bar = int(72 * display_scaling / 100) #71 is chrome tab and address bar height when scaling is 100%
+    if info_bar_shown:
+        tab_bar = int(116 * display_scaling / 100)
+    else:
+        tab_bar = int(72 * display_scaling / 100)  # 72 is chrome tab and address bar height when scaling is 100%
     x = _get_location_in_pixel(driver, element, tab_bar, display_scaling)["x"]
     y = _get_location_in_pixel(driver, element, tab_bar, display_scaling)["y"] + tab_bar
     pyautogui.moveTo(x, y, duration=1)
@@ -37,8 +40,8 @@ def _get_location_in_pixel(driver, element, tab_bar_height, scaling):
     horizontal_scroll_size = 0
     if _is_vertical_scroll_on_screen(driver):
         vertical_scroll_size = scroll_size
-    # if _is_horizontal_scroll_on_screen(driver):
-    #     horizontal_scroll_size = scroll_size
+    if _is_horizontal_scroll_on_screen(driver):
+        horizontal_scroll_size = scroll_size
     x_location_in_pixels = (x_location_inside_of_browser + element.size["width"]/2)*(GetSystemMetrics(0)-vertical_scroll_size)/(int(driver.find_element_by_tag_name("html").get_attribute("clientWidth")))
     monitor_info = GetMonitorInfo(MonitorFromPoint((0, 0)))
     monitor_area = monitor_info.get("Monitor")
@@ -50,21 +53,6 @@ def _get_location_in_pixel(driver, element, tab_bar_height, scaling):
     return {'x': x_location_in_pixels, 'y': y_location_in_pixels}
 
 
-def _is_vertical_scroll_on_screen(driver):
-    """
-    Checks if scroll is shown
-
-    :param driver: WebDriver
-    :return: bool
-    """
-    vertical_scroll = False
-    body_height = driver.execute_script("return document.getElementsByTagName('body')[0].clientHeight")
-    inner_height = driver.execute_script("return window.innerHeight")
-    if body_height > inner_height:
-        vertical_scroll = True
-    return vertical_scroll
-
-
 def _is_horizontal_scroll_on_screen(driver):
     """
     Checks if scroll is shown
@@ -73,9 +61,28 @@ def _is_horizontal_scroll_on_screen(driver):
     :return: bool
     """
     horizontal_scroll = False
+    body_height = driver.execute_script("return document.getElementsByTagName('body')[0].clientHeight")
+    inner_height = driver.execute_script("return window.innerHeight")
+    if body_height != inner_height:
+        horizontal_scroll = True
+    print("horizontal")
+    print(horizontal_scroll)
+    return horizontal_scroll
+
+
+def _is_vertical_scroll_on_screen(driver):
+    """
+    Checks if scroll is shown
+
+    :param driver: WebDriver
+    :return: bool
+    """
+    vertical_scroll = False
     body_width = driver.execute_script("return document.getElementsByTagName('body')[0].clientWidth")
     inner_width = driver.execute_script("return window.innerWidth")
-    if body_width > inner_width:
-        horizontal_scroll = True
-    return horizontal_scroll
+    if body_width != inner_width:
+        vertical_scroll = True
+    print("vertical")
+    print(vertical_scroll)
+    return vertical_scroll
 
