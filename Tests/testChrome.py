9a3e76d9b22ec7b225c170b48df801979226ae39
smirad91 <smirad91@gmail.com>
2019-08-18 14:40:35 +0200

    update

--- Tests/testChrome.py
+++ Tests/testChrome.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium_move_cursor.MouseActions import move_to_element_chrome
+from selenium_move_cursor.MouseActions import move_to_element
 
 driver = webdriver.Chrome()
 
@@ -10,4 +10,4 @@ driver.get("https://www.google.com/")
 element = driver.find_elements_by_css_selector("input[class='gNO89b']")[1]
 
 
-move_to_element_chrome(driver, element, display_scaling=100, chrome_info_bar_shown=True)
+move_to_element(driver, element, display_scaling=100, chrome_info_bar_shown=True)
--- Tests/testFirefox.py
+++ Tests/testFirefox.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium_move_cursor.MouseActions import move_to_element_chrome
+from selenium_move_cursor.MouseActions import move_to_element
 
 
 driver = webdriver.Firefox()
@@ -12,4 +12,4 @@ driver.get("https://www.google.com/")
 element = driver.find_elements_by_css_selector("input[class='gNO89b']")[1]
 
 
-move_to_element_chrome(driver, element, display_scaling=100)
+move_to_element(driver, element, display_scaling=100)
